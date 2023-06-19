9db9f8bdf73771d6fc3ee3c20edcf04ab7985b24
ph-alaa <Alaa_lafi333@hotmail.com>
2023-06-19 20:57:11 +0300

    CC03

---
+++ CodeChallengesTests/test03.cs
@@ -0,0 +1,26 @@
+﻿using data_structures_and_algorithms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeChallengesTests
+{
+    public class test03
+    {
+        [Theory]
+        [InlineData(new int[] { 0,1, 2, 3, 4, 5 },4,4)]
+        [InlineData(new int[] {-1,0, 5,7,11,25 }, 0, 1)]
+        [InlineData(new int[] {}, 4, -1)] //empty array 
+        [InlineData(new int[] { 0, 1, 2, 3, 4, 5 }, 6, -1)]//target not exist in array
+
+
+        public void TestBinarySearchReturnNum(int []arr,int target, int expected)
+        {
+            int[] arrTest = arr;
+            int result = CC03.BinarySearch( arrTest,target);
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ data-structures-and-algorithms/CC03.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace data_structures_and_algorithms
+{
+    public class CC03
+    {
+        public static int BinarySearch(int[] arr, int target)
+        {
+            int left = 0;
+            int right = arr.Length - 1;
+            while (left <= right)
+            {
+                int m = (left + right) / 2;
+                if (target == arr[m])
+                    return m;
+                else if (arr[m] > target)
+                    right = m - 1;
+                else
+                    left = m + 1;
+            }
+
+            return -1;
+        }
+    }
+}
+
+/*
+ Recurion :
+ public static int BinarySearch(int left, int right, int target, int[] arr)
+        {
+            if (left <= right)
+            {
+                int m = (left + right) / 2;
+                if (target == arr[m])
+                    return m;
+                else if (arr[m] > target)
+                    return BinarySearch(left, m - 1, target, arr);
+                else
+                    return BinarySearch(m + 1, right, target, arr);
+            }
+            else
+                return -1;
+        }
+*/
