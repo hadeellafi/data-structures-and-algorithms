523c34299827d9510ab65ba402eb381d97a87989
ph-alaa <Alaa_lafi333@hotmail.com>
2023-06-24 18:36:25 +0300

    CC05

---
+++ CodeChallengesTests/test05s.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using data_structures_and_algorithms;
+using Xunit;
+
+namespace CodeChallengesTests
+{
+    public class test05s
+    {
+        [Fact]
+        public void TestInsertEmptyList()
+        {
+            List testList=new List();
+            bool result = testList.IsEmpty();
+            Assert.Equal(result, true);
+        }
+        [Fact]
+        public void TestInsertIntoEmptyList()
+        {
+            List testList = new List();
+
+            testList.InsertFirst(10);
+
+            Assert.Equal(10, testList.head.data);
+            Assert.Equal(testList.head, testList.tail);
+        }
+        [Fact]
+        public void TestInsertIntoNonEmptyList()//Can properly insert into the linked list
+        {
+            List testList = new List();
+            testList.InsertFirst(10);
+            testList.InsertFirst(20);
+            Assert.Equal(20, testList.head.data);
+            Assert.Equal(10, testList.tail.data);
+
+        }
+        [Fact]
+        public void TestHead()//The head property will properly point to the first node
+        {
+            List testList = new List();
+            testList.InsertFirst(10);
+            testList.InsertFirst(20);
+            testList.InsertFirst(30);
+            Assert.Equal(30, testList.head.data);
+        }
+        [Fact]
+        public void TestInsertMultipleNodes()
+        {
+            List testList = new List();
+
+            testList.InsertFirst(50);
+            testList.InsertFirst(40);
+            testList.InsertFirst(30);
+            
+
+            Assert.Equal(30, testList.head.data);
+            Assert.Equal(50, testList.tail.data);
+            Assert.Equal(40, testList.head.next.data);
+          
+            Assert.Null(testList.head.next.next.next);
+        }
+
+        [Theory]
+        [InlineData(10, true)]
+        [InlineData(20, true)]
+        [InlineData(30, true)]
+        [InlineData(40, false)]
+        [InlineData(50, false)]
+        public void TestIsIncludeReturnBool(int value, bool expected)//test IsIncludefunction
+        {
+            List testList = new List();
+            testList.InsertFirst(30);
+            testList.InsertFirst(20);
+            testList.InsertFirst(10);
+
+            bool actual = testList.IsInclude(value);
+
+            
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void TestToStringRetrunString()
+        {
+            List testList = new List();
+            testList.InsertFirst(10);
+            testList.InsertFirst(20);
+            testList.InsertFirst(30);
+
+            string result = testList.ToString();
+
+            Assert.Equal("{30}->{20}->{10}->Null", result);
+        }
+
+    }
+
+}
+/*
+
+Can properly return a collection of all the values that exist in the linked list*/
---
+++ data-structures-and-algorithms/CC05.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace data_structures_and_algorithms
+{
+    internal class CC05
+    {
+    }
+
+    public class Node
+    {
+        public int data;
+        public Node next;
+
+        public Node()
+        {
+            next = null;
+        }
+
+        public Node(int d, Node n = null)
+        {
+            data = d;
+            next = n;
+        }
+    }
+    public class List
+    {
+        public Node head { get; set; }
+        public Node tail { get; set; }
+
+        public List()
+        {
+            head = null;
+            tail = null;
+        }
+        public bool IsEmpty()
+        {
+            return head == null;
+        }
+        public void InsertEnd(int el)
+        {
+            if (IsEmpty())
+                head = tail = new Node(el);
+            else
+            {
+                tail.next = new Node(el);
+                tail = tail.next;
+            }
+            ///tail=tail.next=new node(el);
+        }
+        public void InsertFirst(int el)
+        {
+            if (IsEmpty())
+                head = tail = new Node(el);
+            else
+            {
+                head = new Node(el, head);
+            }
+        }
+        public bool IsInclude(int el)
+        {
+            for(Node t=head;t!=null;t=t.next)
+            {
+                if(t.data==el)
+                    return true;
+            }
+            return false;
+        }
+        public string ToString()
+        {
+            string sum = "";
+            for (Node t = head; t != null; t = t.next)
+            {
+                sum += "{" + t.data + "}" + "->";
+            }
+            return sum+"Null";
+        }
+    }
+}
+   
