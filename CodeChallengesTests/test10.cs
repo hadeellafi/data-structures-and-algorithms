a7b9954a507d27eaa05a19c281fa1f3407832854
ph-alaa <Alaa_lafi333@hotmail.com>
2023-07-12 00:19:53 +0300

    CC10

---
+++ CodeChallengesTests/test10.cs
@@ -0,0 +1,182 @@
+﻿using data_structures_and_algorithms;
+
+
+namespace CodeChallengesTests
+{
+    public class test10
+    {
+        [Fact]
+        public void TestPushStack()
+        {
+            Stack test =new Stack();
+            test.Push(1);
+
+            Assert.Equal(1, test.Peek());
+        }
+        [Fact]
+        public void TestPushMultiIntoStack()
+        {
+            Stack test = new Stack();
+            test.Push(1);
+            test.Push(2);
+            test.Push(3);
+
+            Assert.Equal(3, test.Peek());
+            
+
+        }
+        [Fact]
+        public void TestPopOffStack()
+        {
+            Stack test = new Stack();
+            test.Push(1);
+            test.Push(2);
+            test.Push(3);
+
+            Assert.Equal(3, test.Pop());
+            Assert.Equal(2, test.Peek());
+
+
+        }
+        [Fact]
+        public void TestPopReachEmptyStack()
+        {
+            Stack test = new Stack();
+            test.Push(1);
+            test.Push(2);
+
+            test.Pop();
+            test.Pop();
+
+            Assert.True(test.IsEmpty());
+
+
+        }
+        [Fact]
+        public void TestPeekStack()
+        {
+            Stack test = new Stack();
+            test.Push(1);
+            test.Push(2);
+
+
+            Assert.Equal(2,test.Peek());
+
+
+        }
+
+        [Fact]
+        public void TestInstantiateEmptyStack()
+        {
+            Stack test = new Stack();
+
+            Assert.True(test.IsEmpty());
+
+        }
+        [Fact]
+        public void TestPeekOnEmptyStackThrowException()
+        {
+            Stack test = new Stack();
+
+            Assert.Throws<InvalidOperationException>(() => test.Peek());
+        }
+        [Fact]
+        public void TestPopOnEmptyStackThrowException()
+        {
+            Stack test = new Stack();
+
+            Assert.Throws<InvalidOperationException>(() => test.Pop());
+        }
+        ///////////queue tests
+
+        [Fact]
+        public void TestEnqueueIntoQueue()
+        {
+            Queue queue = new Queue();
+
+            queue.Enqueue(10);
+
+            Assert.Equal(10,queue.Peek());
+        }
+
+        [Fact]
+        public void TestEnqueueMultipleIntoQueue()
+        {
+            Queue queue = new Queue();
+
+            queue.Enqueue(10);
+            queue.Enqueue(20);
+            queue.Enqueue(30);
+
+            Assert.True(!queue.IsEmpty());
+            Assert.Equal(10, queue.Peek());
+        }
+
+        [Fact]
+        public void TestDequeueFromQueue()
+        {
+            Queue queue = new Queue();
+            queue.Enqueue(10);
+            queue.Enqueue(20);
+            queue.Enqueue(30);
+
+
+            Assert.Equal(10, queue.Dequeue());
+            Assert.Equal(20, queue.Peek());
+        }
+
+        [Fact]
+        public void TestPeekQueue()
+        {
+            Queue queue = new Queue();
+            queue.Enqueue(10);
+            queue.Enqueue(20);
+            queue.Enqueue(30);
+
+            Assert.Equal(10, queue.Peek());
+        }
+
+        [Fact]
+        public void TestEmptyQueueMultipleDequeues()
+        {
+            Queue queue = new Queue();
+            queue.Enqueue(10);
+            queue.Enqueue(20);
+            queue.Enqueue(30);
+
+            queue.Dequeue();
+            queue.Dequeue();
+            queue.Dequeue();
+
+            Assert.True(queue.IsEmpty());
+        }
+
+        [Fact]
+        public void TestInstantiateEmptyQueue()
+        {
+            Queue queue = new Queue();
+
+            Assert.True(queue.IsEmpty());
+        }
+
+        [Fact]
+        public void TestDequeueOnEmptyQueueThrowException()
+        {
+            Queue queue = new Queue();
+
+            Assert.Throws<InvalidOperationException>(() => queue.Dequeue());
+        }
+
+        [Fact]
+        public void TestPeekOnEmptyQueueThrowException()
+        {
+            Queue queue = new Queue();
+
+            Assert.Throws<InvalidOperationException>(() => queue.Peek());
+        }
+    }
+
+}
+
+/*
+*/
---
+++ data-structures-and-algorithms/CC10.cs
@@ -0,0 +1,195 @@
+﻿/*Note this file hold 2 implemention of both Stack & Queue:
+ implement Stack & Queue using list as underlying data storage mechanism 
+ implement Stack & Queue without using list as underlying data storage mechanism(as comment at the end of fill)
+ and test woks correctly on both
+*/
+
+namespace data_structures_and_algorithms
+{
+    //using list as underlying data storage mechanism 
+    public class Stack
+    {
+        private List list;
+
+        public Stack()
+        {
+            list = new List();
+        }
+
+        public void Push(int el)
+        {
+            list.InsertFirst(el);
+        }
+
+        public int Pop()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("Cannot pop from an empty stack.");
+            }
+
+            int output = list.head.data;
+            list.head = list.head.next;
+
+            return output;
+        }
+
+        public int Peek()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("Cannot peek an empty stack.");
+            }
+
+            return list.head.data;
+        }
+
+        public bool IsEmpty()
+        {
+            return list.IsEmpty();
+        }
+    }
+
+    public class Queue
+    {
+        private List list;
+
+        public Queue()
+        {
+            list = new List();
+        }
+
+        public void Enqueue(int el)
+        {
+            list.InsertEnd(el);
+        }
+
+        public int Dequeue()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("Cannot dequeue from an empty queue.");
+            }
+
+            int output = list.head.data;
+            list.head = list.head.next;
+
+            return output;
+        }
+
+        public int Peek()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("Cannot peek an empty queue.");
+            }
+
+            return list.head.data;
+        }
+
+        public bool IsEmpty()
+        {
+            return list.IsEmpty();
+        }
+    }
+}
+//////// implement Stack & Queue without using list as underlying data storage mechanism:
+
+/* 
+    public class Stack
+    {
+        public Node Top { get; set; }
+        private int count;
+        public Stack()
+        {
+            Top = null;
+            count = 0;
+
+        }
+        public bool IsEmpty()
+        {
+            return count == 0;
+        }
+        public void Push(int el)
+        {
+            if (IsEmpty())
+                Top = new Node(el);
+            else
+                Top = new Node(el, Top);
+
+
+            count++;
+        }
+        public int Pop()
+        {
+            if (IsEmpty())
+                throw new InvalidOperationException("Stack is empty");
+
+            int output = Top.data;
+            Top = Top.next;
+            count--;
+            return output;
+
+
+        }
+        public int Peek()
+        {
+            if (IsEmpty())
+                throw new InvalidOperationException("Stack is empty");
+
+            return Top.data;
+
+
+        }
+
+    }
+    public class Queue
+    {
+        public Node Front { get; set; }
+        public Node Rear { get; set; }
+
+        private int count;
+        public Queue()
+        {
+            Front = Rear = null;
+            count = 0;
+
+        }
+        public bool IsEmpty()
+        {
+            return count == 0;
+        }
+        public void Enqueue(int el)
+        {
+            if (IsEmpty())
+                Front = Rear = new Node(el);
+            else
+                Rear = Rear.next = new Node(el);
+
+
+            count++;
+        }
+        public int Dequeue()
+        {
+            if (IsEmpty())
+                throw new InvalidOperationException("Queue is empty");
+
+            int output = Front.data;
+            Front = Front.next;
+            count--;
+            return output;
+
+        }
+        public int Peek()
+        {
+            if (IsEmpty())
+                throw new InvalidOperationException("Queue is empty");
+
+            return Front.data;
+
+        }
+    }
+
+
+    
+*/
