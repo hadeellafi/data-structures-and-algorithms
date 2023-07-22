c46205de935e49cf8693c547b7e06bf126ab6f28
ph-alaa <Alaa_lafi333@hotmail.com>
2023-07-22 22:29:31 +0300

    CC13

---
+++ CodeChallengesTests/test13.cs
@@ -0,0 +1,33 @@
+﻿using data_structures_and_algorithms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CodeChallengesTests
+{
+    public class test13
+    {
+        [Theory]
+        [InlineData("{}", true)]
+        [InlineData("{}(){}", true)]
+        [InlineData("()[[Extra Characters]]", true)]
+        [InlineData("(){}[[]]", true)]
+        [InlineData("{}{Code}[Fellows](())", true)]
+        [InlineData("", true)] // Empty string is balanced
+        [InlineData("[({}]", false)]
+        [InlineData("(]", false)]
+        [InlineData("{(})", false)]
+        [InlineData("{",false)]
+        [InlineData(")", false)]
+
+        public void testValidateBrackets(string input, bool expectedOutput)
+        {
+            var validator = new CC13();
+            Assert.Equal(expectedOutput,validator.ValidateBrackets(input));
+        }
+    }
+}
---
+++ data-structures-and-algorithms/CC13.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace data_structures_and_algorithms
+{
+    public class CC13
+    {
+       public bool ValidateBrackets(string input)
+        {
+            Stack<char> inputs = new Stack<char>();
+            foreach (char c in input)
+            {
+                if (c == '{' || c == '(' || c == '[')
+                {
+                    inputs.Push(c);
+                }
+                else
+                {
+                    switch (c)
+                    {
+                        case ')':
+                            if (inputs.Count > 0 && inputs.Peek() == '(')
+                                inputs.Pop();
+                            
+                            else
+                                return false;
+                            
+                            break;
+                        case '}':
+                            if (inputs.Count > 0 && inputs.Peek() == '{')
+
+                                inputs.Pop(); 
+                            
+                            else
+                            
+                                return false;
+                            
+                            break;
+                        case ']':
+                            if (inputs.Count > 0 && inputs.Peek() == '[')
+                            {
+                                inputs.Pop();
+                            }
+                            else
+                            {
+                                return false;
+                            }
+                            break;
+                    }
+                }
+            }
+            return inputs.Count == 0;
+        }
+    }
+}
