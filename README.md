278c372dafdbcb7414121adf3d939d13ecb1bb85
HadeelLafi <hadeellafi9@gmail.com>
2023-09-24 01:55:58 +0300

    CC32

--- README.md
+++ README.md
@@ -48,6 +48,9 @@ Challenge 09 linked-list-reverse
 
 [Challenge 31 # Hashmap repeated word](./readMeFiles/CC31.md)
 
+[Challenge 31 # tree intersection](./readMeFiles/CC32.md)
+
+
 
 
 ---
--- data-structures-and-algorithms/TreeIntersection.cs
+++ data-structures-and-algorithms/TreeIntersection.cs
@@ -3,42 +3,54 @@ using System.Collections.Generic;
 
 namespace data_structures_and_algorithms
 {
-    public class TreeIntersection
+    public class TreeNode
     {
-        public static HashSet<int> FindCommonValues(Node<int> root1, Node<int> root2)
+        public int Value;
+        public TreeNode Left;
+        public TreeNode Right;
+
+        public TreeNode(int value)
         {
-            HashSet<int> commonValues = new HashSet<int>();
-            Dictionary<int, bool> valuesMap = new Dictionary<int, bool>();
-
-            void TraverseTree(Node<int> node)
-            {
-                if (node == null)
-                    return;
-
-                valuesMap[node.Value] = true;
-                TraverseTree(node.Left);
-                TraverseTree(node.Right);
-            }
-
-            TraverseTree(root1);
-
-            void CheckCommonValues(Node<int> node)
-            {
-                if (node == null)
-                    return;
-
-                if (valuesMap.ContainsKey(node.Value))
-                {
-                    commonValues.Add(node.Value);
-                }
-
-                CheckCommonValues(node.Left);
-                CheckCommonValues(node.Right);
-            }
-
-            CheckCommonValues(root2);
-
-            return commonValues;
+            Value = value;
+            Left = null;
+            Right = null;
         }
     }
+
+    public class TreeIntersection
+    {
+        public HashSet<int> TreeIntersectionMethod(TreeNode root1, TreeNode root2)
+        {
+            HashSet<int> result = new HashSet<int>();
+            HashTable hashTable = new HashTable(20);
+
+            Traverse(root1, hashTable);
+            CheckIntersection(root2, hashTable, result);
+
+            return result;
+        }
+
+        private void Traverse(TreeNode node, HashTable hashTable)
+        {
+            if (node == null)
+                return;
+
+            hashTable.Set(node.Value.ToString(), node.Value.ToString());
+            Traverse(node.Left, hashTable);
+            Traverse(node.Right, hashTable);
+        }
+
+        private void CheckIntersection(TreeNode node, HashTable hashTable, HashSet<int> result)
+        {
+            if (node == null)
+                return;
+
+            if (hashTable.Has(node.Value.ToString()))
+                result.Add(node.Value);
+
+            CheckIntersection(node.Left, hashTable, result);
+            CheckIntersection(node.Right, hashTable, result);
+        }
+    }
+
 }
