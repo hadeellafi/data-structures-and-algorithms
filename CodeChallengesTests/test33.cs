81004ba1946522a5ef35a22422128918d532345f
HadeelLafi <hadeellafi9@gmail.com>
2023-09-26 01:34:37 +0300

    CC33

---
+++ CodeChallengesTests/test33.cs
@@ -0,0 +1,50 @@
+﻿using data_structures_and_algorithms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeChallengesTests
+{
+    public class test33
+    {
+     
+        [Fact]
+        public void TestLeftJoin_WithCommonKeys()
+        {
+            var table1 = new HashTable(10);
+            var table2 = new HashTable(10);
+            table1.Set("key1", "synonym1");
+            table2.Set("key1", "antonym1");
+            var leftJoin = new HashMapLeftJoin();
+            var result = leftJoin.LeftJoin(table1, table2);
+            Assert.Equal(new List<string> { "key1", "synonym1", "antonym1" }, result[0]);
+        }
+
+        [Fact]
+        public void TestLeftJoin_WithNoCommonKeys()
+        {
+            var table1 = new HashTable(10);
+            var table2 = new HashTable(10);
+            table1.Set("key1", "synonym1");
+            table2.Set("key2", "antonym2");
+            var leftJoin = new HashMapLeftJoin();
+            var result = leftJoin.LeftJoin(table1, table2);
+            Assert.Equal(new List<string> { "key1", "synonym1", "NULL" }, result[0]);
+        }
+
+        [Fact]
+        public void TestLeftJoin_WithEmptySecondTable()
+        {
+            var table1 = new HashTable(10);
+            var table2 = new HashTable(10);
+            table1.Set("key1", "synonym1");
+            var leftJoin = new HashMapLeftJoin();
+            var result = leftJoin.LeftJoin(table1, table2);
+            Assert.Equal(new List<string> { "key1", "synonym1", "NULL" }, result[0]);
+        }
+    }
+
+}
+
---
+++ data-structures-and-algorithms/CC33.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace data_structures_and_algorithms
+{
+    public class HashMapLeftJoin
+    {
+        public List<List<string>> LeftJoin(HashTable table1, HashTable table2)
+        {
+            List<List<string>> left = new List<List<string>>();
+
+            foreach (var key in table1.Keys())
+            {
+                string value1 = table1.Get(key);
+                
+                string value2 = table2.Get(key);
+                if (value2 == null)
+                    value2 = "NULL";
+
+
+                List<string> data = new List<string> { key, value1, value2 };
+                left.Add(data);
+            }
+
+            return left;
+        }
+    }
+}
