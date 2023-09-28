73350d36cece5d8d06f4e12da6f8ae1d14d7d5c0
HadeelLafi <hadeellafi9@gmail.com>
2023-09-28 10:48:54 +0300

    cc34

---
+++ CodeChallengesTests/test34.cs
@@ -0,0 +1,23 @@
+﻿using data_structures_and_algorithms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeChallengesTests
+{
+    public class test34
+    {
+        [Theory]
+        [InlineData("In a galaxy far far away", "far")]
+        [InlineData("Taco cat ate a taco", "taco")]
+        [InlineData("No. Try not. Do or do not. There is no try.", "no")]
+        public void MostReapetedWordTest(string input, string expected)
+        {
+            CC34 cc34 = new CC34();
+            var result = cc34.MostReapetedWord(input);
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ data-structures-and-algorithms/CC34.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace data_structures_and_algorithms
+{
+    public class CC34
+    {
+        public string MostReapetedWord(string paragraph)
+        {
+            string[] words = Regex.Split(paragraph.ToLower(), @"[\W_]+");
+            Hashtable hashtable = new Hashtable();
+            foreach (string word in words)
+            {
+                if (hashtable.ContainsKey(word))
+                {
+                    hashtable[word]= (int)hashtable[word]+1;
+                }
+                else
+                {
+                    hashtable.Add(word, 1);
+                }
+            }
+            int count = 0;
+            string mostReapetedWord = null;
+            foreach (DictionaryEntry item in hashtable)
+            {
+                if ((int)item.Value > count)
+                {
+                    count = (int)item.Value;
+                    mostReapetedWord = item.Key.ToString();
+                }
+            }
+            return mostReapetedWord;
+        }
+    }
+}
