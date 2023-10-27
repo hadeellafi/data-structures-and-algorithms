d8f0a199b635076cad976f9f1e7305023a27c572
HadeelLafi <hadeellafi9@gmail.com>
2023-10-28 00:06:46 +0300

    finalChallenge

---
+++ CodeChallengesTests/test39.cs
@@ -0,0 +1,57 @@
+﻿using data_structures_and_algorithms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeChallengesTests
+{
+    public class GraphConversionTests
+    {
+        [Fact]
+        public void TestConvertMatrixToList_SingleNodeGraph()
+        {
+            string[] nodes = new string[] { "a" };
+            bool[][] matrix = new bool[][] { new bool[] { false } };
+            var adjacencyList = CC39.ConvertMatrixToList(nodes, matrix);
+            Assert.Single(adjacencyList);
+            Assert.Empty(adjacencyList["a"]);
+        }
+
+        [Fact]
+        public void TestConvertMatrixToList_LinearGraph()
+        {
+            string[] nodes = new string[] { "a", "b", "c" };
+            bool[][] matrix = new bool[][]
+            {
+            new bool[] { false, true, false },
+            new bool[] { true, false, true },
+            new bool[] { false, true, false }
+            };
+            var adjacencyList = CC39.ConvertMatrixToList(nodes, matrix);
+            Assert.Equal(3, adjacencyList.Count);
+            Assert.Single(adjacencyList["a"]);
+            Assert.Equal(2, adjacencyList["b"].Count);
+            Assert.Single(adjacencyList["c"]);
+        }
+
+        [Fact]
+        public void TestConvertMatrixToList_BranchingGraph()
+        {
+            string[] nodes = new string[] { "a", "b", "c" };
+            bool[][] matrix = new bool[][]
+            {
+            new bool[] { false, true, true },
+            new bool[] { true, false, true },
+            new bool[] { true, true, false }
+            };
+            var adjacencyList = CC39.ConvertMatrixToList(nodes, matrix);
+            Assert.Equal(3, adjacencyList.Count);
+            Assert.Equal(2, adjacencyList["a"].Count);
+            Assert.Equal(2, adjacencyList["b"].Count);
+            Assert.Equal(2, adjacencyList["c"].Count);
+        }
+    }
+
+}
---
+++ data-structures-and-algorithms/CC39.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace data_structures_and_algorithms
+{
+     public class CC39 
+    {
+        static public Dictionary<string, List<string>> ConvertMatrixToList(string[] nodes, bool[][] matrix)
+        {
+            var adjacencyList = new Dictionary<string, List<string>>();
+
+            for (int i = 0; i < nodes.Length; i++)
+            {
+                adjacencyList[nodes[i]] = new List<string>();
+
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    if (matrix[i][j])
+                    {
+                        adjacencyList[nodes[i]].Add(nodes[j]);
+                    }
+                }
+            }
+
+            return adjacencyList;
+        }
+
+    }
+}
